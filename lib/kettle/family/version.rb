a121c5086291053ff947e319ed2319536e5c8a91
Peter H. Boling <peter.boling@gmail.com>
2026-06-24 08:52:46 -0600

    🔖 Prepare release v0.1.12

--- lib/kettle/family/version.rb
+++ lib/kettle/family/version.rb
@@ -3,7 +3,7 @@
 module Kettle
   module Family
     module Version
-      VERSION = "0.1.11"
+      VERSION = "0.1.12"
     end
     VERSION = Version::VERSION # Traditional Constant Location
   end
